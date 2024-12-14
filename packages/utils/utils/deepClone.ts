c90a2c2fb4dde8b0227b2fa84b1c391233336728
tangjiaqiang2 <tangjiaqiang2@yy.com>
2024-12-14 23:34:54 +0800

    per: add features

---
+++ packages/utils/utils/deepClone.ts
@@ -0,0 +1,25 @@
+/**
+ * 
+ * @param obj 原始对象
+ * @param hash WeakMap 避免循环引用
+ * @returns 
+ */
+
+export function deepClone(obj, hash = new WeakMap()) {
+    if (!obj) return null
+    if (obj instanceof Date) return new Date(obj);
+    if (obj instanceof RegExp) return new RegExp(obj);
+
+    if (typeof obj !== "object") return obj;
+    if (hash.has(obj)) return hash.get(obj);
+    let cloneObj = new obj.constructor() // 负责拷贝的类型
+
+    hash.set(obj, cloneObj)
+
+    for (const key in obj) {
+        if (Object.prototype.hasOwnProperty.call(obj, key)) {
+            cloneObj[key] = deepClone(obj[key], hash)
+        }
+    }
+    return cloneObj
+}
---
+++ packages/utils/utils/enhance.ts
@@ -0,0 +1,49 @@
+/**
+ * @description 函数重载的一种实现
+ * @returns 
+ */
+export function enhance() {
+    const map = new Map()
+    // 主调用函数
+    const enhanceFunction = (...args) => {
+        const key = args.map((arg) => typeof arg).join('-')
+        console.log(key, '--------')
+        const fn = map.get(key)
+        if (fn && typeof fn === 'function') {
+            return fn(...args)
+        } else {
+            throw new Error('not found funcion')
+        }
+    }
+
+    // 注册函数
+    /**
+     * 
+     * @param args 函数参数
+     * @param fn 函数体
+     * @returns 
+     */
+    enhanceFunction.register = (...args) => (fn) => {
+        const key = args.join('-')
+        console.log(key)
+        // map[key] = fn
+        map.set(key, fn)
+    }
+    
+    return enhanceFunction
+
+}
+
+// const myFun = enhance()
+// myFun.register('string')((a) => {
+//     console.log(a)
+// })
+// myFun.register('number', 'number')((a,b) => {
+//     console.log(a+b)
+// })
+
+// myFun('testetst')
+// myFun(3,44)
+
+
+

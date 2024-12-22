5f5a892543d565e0f2b9cb472b836f7044174107
tangjiaqiang2 <tangjiaqiang2@yy.com>
2024-12-22 19:44:24 +0800

    perf: lint

--- packages/utils/utils/promiserace.ts
+++ packages/utils/utils/promiserace.ts
@@ -1,11 +1,11 @@
 export function promiseRace(promises) {
-    return new Promise((resolve, reject) => {
-        // 遍历所有传入的 Promise
-        for (const promise of promises) {
-            // 将每个 Promise 转换为 Promise 对象
-            Promise.resolve(promise)
-                .then(resolve)  // 第一个成功则调用 resolve
-                .catch(reject); // 第一个失败则调用 reject
-        }
-    });
+  return new Promise((resolve, reject) => {
+    // 遍历所有传入的 Promise
+    for (const promise of promises) {
+      // 将每个 Promise 转换为 Promise 对象
+      Promise.resolve(promise)
+        .then(resolve) // 第一个成功则调用 resolve
+        .catch(reject); // 第一个失败则调用 reject
+    }
+  });
 }

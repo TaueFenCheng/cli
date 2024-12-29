439aa14952430043ca4979a1a2cd32c8471bb807
tangjiaqiang2 <tangjiaqiang2@yy.com>
2024-12-29 23:32:01 +0800

    chore: upda compose

--- packages/utils/utils/compose.ts
+++ packages/utils/utils/compose.ts
@@ -3,8 +3,23 @@
  * @description 组合函数
  */
 
+// 从右往左
 export function compose(fns: Function[]) {
   return (val: any) => {
     return fns.reduceRight((arg, fn) => fn(arg), val);
   };
+}
+
+/**
+ * 
+ * @param fns 
+ * @returns 
+ * @description reduce 实现 compose 函数 从右往左
+ */
+export function compose2(fns:any[]){
+  return (...args)=>{
+    return fns.reduce((a,b)=>{
+      return a(b(...args))      
+    },args)
+  }
 }

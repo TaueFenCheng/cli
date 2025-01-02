b40fc4ecb9835dcfe27a3b12d981a5866d6d1f0d
tangjiaqiang2 <tangjiaqiang2@yy.com>
2025-01-02 21:50:10 +0800

    chore: update promise

---
+++ packages/utils/utils/promiseCurrent.ts
@@ -0,0 +1,20 @@
+async function promiseWithConcurrencyLimit(tasks, limit) {
+  const results = [];
+  const executing = [];
+  for (const task of tasks) {
+    const promise = task();
+    results.push(promise);
+    executing.push(promise);
+    if (executing.length >= limit) {
+      await Promise.race(executing);
+      executing.splice(
+        executing.findIndex((p) => p === promise),
+        1
+      );
+    }
+  }
+  await Promise.all(executing);
+  return Promise.all(results);
+}
+
+export default promiseWithConcurrencyLimit;

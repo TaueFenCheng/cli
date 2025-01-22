077cde71022f7c478bd03d46e468c9e494be4542
tangjiaqiang2 <tangjiaqiang2@yy.com>
2025-01-22 11:23:02 +0800

    perf: lint

--- packages/utils/utils/parcial.ts
+++ packages/utils/utils/parcial.ts
@@ -1,7 +1,9 @@
 /**
  * @description partial 递归
  */
+// eslint-disable-next-line no-unused-vars
 type RecursivePartial<T> = {
   [P in keyof T]?: T[P] extends object ? RecursivePartial<T[P]> : T[P];
 };
+// eslint-disable-next-line no-unused-vars
 type DeepPartial<T> = Partial<{ [P in keyof T]: DeepPartial<T[P]> }>;
